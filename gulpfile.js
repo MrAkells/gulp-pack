6d50c915add97f656a3121bfdb94c5ffd7cf25dd
MrAkells <mrakellsreg@gmail.com>
2022-01-04 12:09:13 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 import { js } from "./gulp/tasks/js.js";
 import { server } from "./gulp/tasks/server.js";
 
@@ -23,7 +24,9 @@ function watcher() {
     gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(html, scss, images, js);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+const mainTasks = gulp.series(fonts, gulp.parallel(html, scss, images, js));
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
