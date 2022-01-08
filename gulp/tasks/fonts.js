c3f881bdaf8a269500ff593d8d5344e85325dace
distruct <qqeyu925@gmail.com>
2022-01-08 17:09:19 +0200

    refactor task Fonts

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -49,53 +49,63 @@ export const fontsStyle = () => {
         if (fontsFiles) {
             if (!fs.existsSync(fontsFile)) {
                 fs.writeFile(fontsFile, "", cb);
-                let newFileOnly;
-                for (var i = 0; i < fontsFiles.length; i++) {
-                    let fontFileName = fontsFiles[i].split(".")[0];
-                    if (newFileOnly !== fontFileName) {
-                        let fontName = fontFileName.split("-")[0]
-                            ? fontFileName.split("-")[0]
-                            : fontFileName;
-                        let fontWeight = fontFileName.split("-")[1]
-                            ? fontFileName.split("-")[1]
-                            : fontFileName;
-                        if (fontWeight.toLowerCase() === "thin") {
-                            fontWeight = 100;
-                        } else if (fontWeight.toLowerCase() === "extralight") {
-                            fontWeight = 200;
-                        } else if (fontWeight.toLowerCase() === "light") {
-                            fontWeight = 300;
-                        } else if (fontWeight.toLowerCase() === "medium") {
-                            fontWeight = 500;
-                        } else if (fontWeight.toLowerCase() === "semibold") {
-                            fontWeight = 600;
-                        } else if (fontWeight.toLowerCase() === "bold") {
-                            fontWeight = 700;
-                        } else if (
-                            fontWeight.toLowerCase() === "extrabold" ||
-                            fontWeight.toLowerCase() === "heavy"
-                        ) {
-                            fontWeight = 800;
-                        } else if (fontWeight.toLowerCase() === "black") {
-                            fontWeight = 900;
-                        } else {
-                            fontWeight = 400;
-                        }
-                        fs.appendFile(
-                            fontsFile,
-                            `@font-face {
-                                font-family: ${fontName};
-                                font-display: swap;
-                                src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normalize;\n}\r\n`,
-                            cb
-                        );
-                        newFileOnly = fontFileName;
-                    }
-                }
+                writeFonts2File(fontsFiles)
+            } else {
+                fs.unlinkSync(fontsFile)
+                fs.writeFile(fontsFile, "", cb);
+                writeFonts2File(fontsFiles)
             }
         }
     });
 
     return app.gulp.src(`${app.path.srcFolder}`);
-    function cb() {}
+
+    function writeFonts2File(fonts2Files) {
+        let newFileOnly;
+        for (var i = 0; i < fonts2Files.length; i++) {
+            let fontFileName = fonts2Files[i].split(".")[0];
+            if (newFileOnly !== fontFileName) {
+                let fontName = fontFileName.split("-")[0]
+                    ? fontFileName.split("-")[0]
+                    : fontFileName;
+                let fontWeight = fontFileName.split("-")[1]
+                    ? fontFileName.split("-")[1]
+                    : fontFileName;
+                if (fontWeight.toLowerCase() === "thin") {
+                    fontWeight = 100;
+                } else if (fontWeight.toLowerCase() === "extralight") {
+                    fontWeight = 200;
+                } else if (fontWeight.toLowerCase() === "light") {
+                    fontWeight = 300;
+                } else if (fontWeight.toLowerCase() === "medium") {
+                    fontWeight = 500;
+                } else if (fontWeight.toLowerCase() === "semibold") {
+                    fontWeight = 600;
+                } else if (fontWeight.toLowerCase() === "bold") {
+                    fontWeight = 700;
+                } else if (
+                    fontWeight.toLowerCase() === "extrabold" ||
+                    fontWeight.toLowerCase() === "heavy"
+                ) {
+                    fontWeight = 800;
+                } else if (fontWeight.toLowerCase() === "black") {
+                    fontWeight = 900;
+                } else {
+                    fontWeight = 400;
+                }
+                fs.appendFile(
+                    fontsFile,
+                    `@font-face {
+    font-family: ${fontName};
+    font-display: swap;
+    src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normalize;\n}\r\n`,
+                    cb
+                );
+                newFileOnly = fontFileName;
+            }
+        }
+    }
+
+    function cb() {
+    }
 };
